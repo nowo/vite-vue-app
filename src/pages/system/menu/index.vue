4c4c6836cb8fd37b88b0adb367a97b16d816f89e
cooj <z8888788@163.com>
2023-11-20 21:45:16 +0800

    fix(other): 删除src文件夹

--- src/pages/system/menu/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>菜单列表</div>
-</template>
-
-<script lang="ts" setup>
-</script>
-
-<style lang="scss" scoped>
-</style>
--- src/types/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-    VITE_APP_TITLE: string
-    // 更多环境变量...
-    VITE_APP_BASEURL: string
-
-    // 是否启用后端路由
-    VITE_REQUEST_ROUTE: boolean | string
-}
