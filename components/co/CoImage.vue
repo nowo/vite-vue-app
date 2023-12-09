f06a754dab3304cdaac9b9b5395cf113134d2fb4
cooj <z8888788@163.com>
2023-12-09 16:52:13 +0800

    feat(deps): 添加vue-draggable-plus用于拖拽功能使用， element-plus图片组件封装，图片加载失败默认显示图标

---
+++ components/co/CoImage.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-image :z-index="10000" :preview-teleported="true" v-bind="$attrs">
+        <template #error>
+            <div class="el-image__error">
+                <i class="i-ep-picture" />
+            </div>
+        </template>
+    </el-image>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    iconSize: {
+        type: Number,
+        default: 16,
+    },
+})
+const iconSize = computed(() => {
+    return `${props.iconSize}px`
+})
+</script>
+
+<style lang="scss" scoped>
+.el-image__error {
+    font-size: v-bind('iconSize');
+}
+</style>
