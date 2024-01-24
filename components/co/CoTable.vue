e09e4f5cbc806e1c1c07b62fca23bc4d2a98deba
cooj <z8888788@163.com>
2024-01-24 09:57:57 +0800

    feat(components): table组件增加列拖拽功能

--- components/co/CoTable.vue
+++ components/co/CoTable.vue
@@ -4,60 +4,67 @@
             <div>
                 <slot />
             </div>
+
             <div v-if="propsData.isTool" class="flex items-center">
                 <el-icon class="tool-icon" :class="{ 'is-loading': defData.isRefresh }">
                     <span class="i-ep-refresh" title="刷新" @click="onRefresh" />
                 </el-icon>
-                <el-dropdown @command="onChangeSize">
-                    <el-icon class="tool-icon">
-                        <svg title="尺寸" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024">
-                            <path fill="currentColor"
-                                d="M840 836H184c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h656c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8m0-724H184c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h656c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8M610.8 378c6 0 9.4-7 5.7-11.7L515.7 238.7a7.14 7.14 0 0 0-11.3 0L403.6 366.3a7.23 7.23 0 0 0 5.7 11.7H476v268h-62.8c-6 0-9.4 7-5.7 11.7l100.8 127.5c2.9 3.7 8.5 3.7 11.3 0l100.8-127.5c3.7-4.7.4-11.7-5.7-11.7H548V378z" />
-                        </svg>
-                    </el-icon>
-                    <template #dropdown>
-                        <el-dropdown-menu>
-                            <el-dropdown-item v-for="(item, index) in defData.sizeList" :key="index" :command="index"
-                                :class="{ 'el-dropdown-menu__item-active': index === size }">
-                                {{ item }}
-                            </el-dropdown-item>
-                        </el-dropdown-menu>
-                    </template>
-                </el-dropdown>
-                <!-- <el-icon class="tool-icon">
+                <client-only>
+                    <el-dropdown @command="onChangeSize">
+                        <el-icon class="tool-icon">
+                            <svg title="尺寸" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
+                                viewBox="0 0 1024 1024">
+                                <path fill="currentColor"
+                                    d="M840 836H184c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h656c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8m0-724H184c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h656c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8M610.8 378c6 0 9.4-7 5.7-11.7L515.7 238.7a7.14 7.14 0 0 0-11.3 0L403.6 366.3a7.23 7.23 0 0 0 5.7 11.7H476v268h-62.8c-6 0-9.4 7-5.7 11.7l100.8 127.5c2.9 3.7 8.5 3.7 11.3 0l100.8-127.5c3.7-4.7.4-11.7-5.7-11.7H548V378z" />
+                            </svg>
+                        </el-icon>
+                        <template #dropdown>
+                            <el-dropdown-menu>
+                                <el-dropdown-item v-for="(item, index) in defData.sizeList" :key="index" :command="index"
+                                    :class="{ 'el-dropdown-menu__item-active': index === size }">
+                                    {{ item }}
+                                </el-dropdown-item>
+                            </el-dropdown-menu>
+                        </template>
+                    </el-dropdown>
+
+                    <!-- <el-icon class="tool-icon">
                     <span class="i-ep-download" title="导出" />
                 </el-icon> -->
-                <el-popover :width="240" popper-class="popover-box" trigger="click">
-                    <template #reference>
-                        <el-icon class="tool-icon">
-                            <span class="i-ep-tools" title="列设置" />
-                        </el-icon>
-                    </template>
-                    <el-scrollbar max-height="660">
-                        <VueDraggable v-model="headerList" tag="ul" class="move-box" handle=".handle" :animation="260">
-                            <li v-for="(item, index) in headerList " :key="index"
-                                class="flex items-center justify-between px8px">
-                                <i class="handle i-ep-rank mr5px inline-block cursor-pointer" />
-                                <div class="flex-1 text-truncate">
-                                    <el-checkbox v-model="item.other.isShow" :label="item.label" @change="onCheckBox" />
-                                </div>
-                                <div class="flex items-center pl5px">
-                                    <div class="pin-icon mr5px" @click="onFixedItem('left', item)">
-                                        <i v-if="item.fixed === 'left'" class="pin-active i-mdi:pin rotate-30" />
-                                        <i v-else class="i-mdi:pin-outline rotate-30" />
+                    <el-popover :width="240" popper-class="popover-box" trigger="click">
+                        <template #reference>
+                            <el-icon class="tool-icon">
+                                <span class="i-ep-tools" title="列设置" />
+                            </el-icon>
+                        </template>
+                        <el-scrollbar max-height="660">
+                            <VueDraggable v-model="headerList" tag="ul" class="move-box" handle=".handle" :animation="260">
+                                <li v-for="(item, index) in headerList " :key="index"
+                                    class="flex items-center justify-between px8px">
+                                    <i class="handle i-ep-rank mr5px inline-block cursor-pointer" />
+                                    <div class="flex-1 text-truncate">
+                                        <el-checkbox v-model="item.other.isShow" :label="item.label" @change="onCheckBox" />
                                     </div>
-                                    <div class="pin-icon" @click="onFixedItem('right', item)">
-                                        <i v-if="item.fixed === 'right'" class="pin-active i-mdi:pin -rotate-30" />
-                                        <i v-else class="i-mdi:pin-outline -rotate-30" />
+                                    <div class="flex items-center pl5px">
+                                        <div class="pin-icon mr5px" @click="onFixedItem('left', item)">
+                                            <i v-if="item.fixed === 'left'" class="pin-active i-mdi:pin rotate-30" />
+                                            <i v-else class="i-mdi:pin-outline rotate-30" />
+                                        </div>
+                                        <div class="pin-icon" @click="onFixedItem('right', item)">
+                                            <i v-if="item.fixed === 'right'" class="pin-active i-mdi:pin -rotate-30" />
+                                            <i v-else class="i-mdi:pin-outline -rotate-30" />
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        </VueDraggable>
-                    </el-scrollbar>
-                </el-popover>
+                                </li>
+                            </VueDraggable>
+                        </el-scrollbar>
+                    </el-popover>
+                </client-only>
             </div>
         </div>
-        <div class="flex-1 overflow-hidden">
+        <VueDraggable v-model="headerList" class="flex-1 overflow-hidden" target=".el-table__header thead tr"
+            :animation="260" :disabled="!propsData.isTool">
+            <!-- <div class="flex-1 overflow-hidden"> -->
             <!-- class="h100%!" -->
             <el-table ref="tableRef" :data="propsData.data" scrollbar-always-on highlight-current-row :size="size"
                 :max-height="tableHeight" v-bind="newAttrs">
@@ -76,7 +83,8 @@
                     </template>
                 </el-table-column>
             </el-table>
-        </div>
+            <!-- </div> -->
+        </VueDraggable>
         <el-pagination v-if="defData.pagination.total" ref="pageRef" v-model:current-page="defData.pagination.page"
             v-model:page-size="defData.pagination.page_size" :small="smallSize" :page-sizes="defData.pagination.page_sizes"
             :total="defData.pagination.total" :pager-count="5" background layout="total, sizes, prev, pager, next, jumper"
@@ -290,6 +298,13 @@ defineExpose({
     height: 100%;
     overflow: auto;
     border: 0;
+
+    :deep(.el-table__header) {
+        .sortable-chosen {
+            border: 0;
+        }
+    }
+
 }
 
 .tool-icon {
